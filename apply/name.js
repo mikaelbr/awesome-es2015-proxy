e320bd7fc418442598ce7befb7e0bd81351d5eee
Mikael Brevik <mikaelbre@gmail.com>
2016-06-28 23:17:52 +0200

    Adds result of decorator example

--- apply/name.js
+++ apply/name.js
@@ -1,9 +1,9 @@
 function decoratee () { return 3 * 1; }
 let fn1 = decorator(decoratee);
-console.log('fn1.name', fn1.name);
+console.log('fn1.name', fn1.name); //=> undefined
 
 let fn2 = proxyDecorator(decoratee);
-console.log('fn2.name', fn2.name);
+console.log('fn2.name', fn2.name); //=> fn2.name decoratee
 
 function decorator (fn) {
   return function (...args) {
