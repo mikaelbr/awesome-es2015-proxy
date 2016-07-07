88500a0c107951ddf54232ad4c3ff6f73fe52e71
Mikael Brevik <mikaelbre@gmail.com>
2016-07-07 09:58:57 +0200

    Adds comment to react example

--- examples/react-api.js
+++ examples/react-api.js
@@ -1,13 +1,14 @@
 const React = require('react');
 const { renderToString } = require('react-dom/server');
 
+// Dynamically created. Does not need to be predefined by React
 const { div, h1 } = requireApi();
 console.log(renderToString(div({
   children: h1({
-    children: `I don't play defence`
+    children: 'Some output'
   })
 })));
-//=> <div data-reactroot="" [...]><h1>I don't play defence</h1></div>
+//=> <div data-reactroot="" [...]><h1>Some output</h1></div>
 
 function requireApi () {
   return new Proxy({}, {
